0e2ea564d0ac5d1c95b2b03362669984c6be5e3a
seya12 <lukas.seyr@gmx.at>
2021-12-23 14:29:11 +0100

    added skeleton for products

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = ({name}) => {
+  return (
+    <div> 
+      <h1>Se Product {name}</h1>
+    </div>
+  )
+}
+
+export default Product
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Product from "./components/Product";
 
 const App = () => {
-  return <h1>Test</h1>
-}
+  return (
+    <div>
+      <Product name="Bubugaga"></Product>
+      <Product name="Test"></Product>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
