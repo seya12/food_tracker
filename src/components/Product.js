d6108f0f614304af09297763a0da4e63ef391517
mgruber <michael_95@gmx.at>
2022-01-01 15:12:48 +0100

    added rating component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Rating from "./Rating";
 
 const Product = ({ name, size, image }) => {
   return (
     <article className="productItem">
       <h4>{name}</h4>
       <img src={image} alt={name}></img>
+      <Rating />
     </article>
   );
 };
---
+++ src/components/Rating.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const Rating = () => {
+  const maxStars = 5;
+
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  return (
+    <div className="rating">
+      {[...Array(maxStars)].map((star, index) => {
+        index++;
+
+        return (
+          <button
+            type="button"
+            key={index}
+            className={index <= (hover || rating) ? "active" : ""}
+            onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
+          >
+            <span className="star">&#9733;</span>
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Rating;
