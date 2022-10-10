c63756027efb7945d65ac14b515c51234298e833
seya12 <lukas.seyr@gmx.at>
2022-10-10 16:07:51 +0200

    add database access and fetch data from there

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -1,45 +1,11 @@
-const data = [
-  {
-    id: 1,
-    name: "MjAMjAM saftiges Huhn an Möhrchen",
-    size: 400,
-    image:
-      "https://www.sandras-tieroase.de/media/image/product/1250/md/mjamjam-saftiges-huhn-an-moehrchen-400g.jpg",
-    rating: 2,
-  },
-  {
-    id: 2,
-    name: "MjAMjAM zarte Ente & Geflügel",
-    size: 200,
-    image:
-      "https://www.sandras-tieroase.de/media/image/product/1247/md/mjamjam-zarte-ente-gefluegel-200g.jpg",
-    rating: 0,
-  },
-  {
-    id: 3,
-    size: 200,
-    image:
-      "https://www.sandras-tieroase.de/media/image/product/4103/md/mjamjam-purer-fleischgenuss-huehnchen-pur-200g.jpg",
-    rating: 5,
-  },
-  {
-    id: 4,
-    name: "MjAMjAM purer Fleischgenuss Pute pur",
-    size: 200,
-    image:
-      "https://www.sandras-tieroase.de/media/image/product/4104/md/mjamjam-purer-fleischgenuss-pute-pur-200g.jpg",
-    rating: 1,
-  },
-  {
-    id: 5,
-    name: "MjAMjAM Mahlzeit mit leckeren Herzen",
-    size: 200,
-    image:
-      "https://www.sandras-tieroase.de/media/image/product/1243/md/mjamjam-mahlzeit-mit-leckeren-herzen-200g.jpg",
-    rating: 1,
-  },
-];
+import { db } from "../db.js";
 
 export const getProducts = (req, res) => {
-  return res.status(200).json(data);
+  const query = "select * from product";
+  db.query(query, (err, data) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).json(data);
+  });
 };
